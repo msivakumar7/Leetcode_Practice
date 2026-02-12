61ed40120b0feab16f988b8fecd85e23a75e8371
msivakumar7 <msivakumarofficial76@gmail.com>
2026-02-12 10:01:37 +0530

    Update MaximumAverageSubarray.c

--- Arrays/slidingWindow/MaximumAverageSubarray.c
+++ Arrays/slidingWindow/MaximumAverageSubarray.c
@@ -1,1 +1,16 @@
+double findMaxAverage(int* nums, int numsSize, int k) {
+    int sum = 0;
+    for(int i = 0;i < k;i++)
+    sum += nums[i];
+    double MaxAverage =(double)sum/k;
+    int s =0;
+    for(int i = k;i < numsSize;i++)
+    {
+        sum = sum - nums[s++] + nums[i];
+        double avg = (double)sum/k;
+        if(avg > MaxAverage)
+            MaxAverage = avg;
+    }
+    return MaxAverage;
 
+}
