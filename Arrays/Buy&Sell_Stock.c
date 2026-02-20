06c9831c3b8e922df21d5f867e1db09d9566e17e
msivakumar7 <msivakumarofficial76@gmail.com>
2026-02-20 09:33:45 +0530

    Update Buy&Sell_Stock.c

--- Arrays/Buy&Sell_Stock.c
+++ Arrays/Buy&Sell_Stock.c
@@ -1,1 +1,24 @@
 
+# Complexity
+- Time complexity:O(n)
+
+- Space complexity:O(1)
+
+# Code
+```c []
+int maxProfit(int* prices, int pricesSize) {
+    int profit = 0,maxprofit = 0,buy = prices[0];
+    for(int i = 1;i < pricesSize;i++)
+    {
+       if(prices[i] < buy)
+            {
+                buy = prices[i];
+                continue;
+            }
+        profit = prices[i] - buy;
+        if(profit > maxprofit)
+            maxprofit = profit;
+    }
+    return maxprofit;  
+}
+```
