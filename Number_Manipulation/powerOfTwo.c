feeb66c6325d9cea7aa45ac86a9bdf6197aeaf88
msivakumar7 <msivakumarofficial76@gmail.com>
2026-02-20 09:29:50 +0530

    Update powerOfTwo.c

--- Number_Manipulation/powerOfTwo.c
+++ Number_Manipulation/powerOfTwo.c
@@ -1,1 +1,21 @@
 
+# Complexity
+- Time complexity: O(logn)
+- Space complexity: O(1)
+
+# Code
+```c []
+bool isPowerOfTwo(int n) {
+    if(n == 1)
+        return true;
+    if(n <= 0)
+         return false;
+    while(n != 1)
+    {
+        if(n%2 != 0)
+            return false;
+        n /= 2;
+    }
+        return true;
+}
+```
