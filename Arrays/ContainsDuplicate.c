5d0ea1aeb3f23def519cb9b83db9141633d7b743
msivakumar7 <msivakumarofficial76@gmail.com>
2026-02-20 10:16:39 +0530

    Create ContainsDuplicate.c

---
+++ Arrays/ContainsDuplicate.c
@@ -0,0 +1,21 @@
+# Complexity
+- Time complexity:O(nlogn)
+- Space complexity:O(1)
+
+# Code
+```c []
+int cmp(const void *a,const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+bool containsDuplicate(int* nums, int numsSize) {
+
+qsort(nums, numsSize, sizeof(int),cmp);
+for(int i = 0;i < numsSize-1;i++)
+{
+    if(nums[i] == nums[i+1])
+    return true;
+}
+return false;
+}
+```
