1d7e6d34a2ef05bc75619d559de4b7fdddbaf405
msivakumar7 <msivakumarofficial76@gmail.com>
2026-02-20 09:59:07 +0530

    Update RemoveDuplicates.c

--- Arrays/RemoveDuplicates.c
+++ Arrays/RemoveDuplicates.c
@@ -1,1 +1,22 @@
+# Complexity
+- Time complexity:O(n)
 
+- Space complexity:O(1)
+
+# Code
+```c []
+int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize <=0)
+        return 0;
+    int duplicate = 1;
+    for(int i = 1;i < numsSize;i++)
+    {
+        if(nums[i] != nums[duplicate-1])
+        {
+            nums[duplicate] = nums[i];
+            duplicate ++;
+        }
+    }
+    return duplicate;
+}
+```
